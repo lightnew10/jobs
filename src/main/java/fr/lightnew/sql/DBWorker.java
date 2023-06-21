e1d7bb6c8b0d5412289b8612f88fd8bc7c557c48
BuildTools <unconfigured@null.spigotmc.org>
2023-06-21 20:21:01 +0200

    optimization of requests with the thread

--- src/main/java/fr/lightnew/sql/DBWorker.java
+++ src/main/java/fr/lightnew/sql/DBWorker.java
@@ -1,43 +1,68 @@
 package fr.lightnew.sql;
 
-import org.bukkit.Chunk;
+import fr.lightnew.Jobs;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.HashMap;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 public class DBWorker extends BukkitRunnable {
 
-    private List<Runnable> todo = new LinkedList<>();
+    private Queue<Runnable> todo = new ArrayDeque<>();
 
     @Override
     public void run() {
         if (todo.isEmpty())
             return;
-        Runnable current = todo.remove(0);
+        Runnable current = todo.poll();
         current.run();
     }
 
-    public void get(Chunk chunk, BlockRequestMethod requestMethod) {
+    public void get(BlockRequestMethod requestMethod) {
+        /*Bukkit.broadcastMessage("Get");
         todo.add(() -> {
-            // TODO : REQUÊTE
-            requestMethod.run(chunk, RequestsSQL.getLocationBlockPosed(chunk));
+            // TODO : REQUEST
+            Bukkit.broadcastMessage("Request 1");
+            requestMethod.run(RequestsSQL.getLocationBlockPosed());
+            Bukkit.broadcastMessage("Request 2");
+        });*/
+        CompletableFuture.runAsync(() -> {
+            // TODO: Requête asynchrone
+            // Effectuez ici votre requête SQL ou toute autre opération longue de manière asynchrone
+            requestMethod.run(RequestsSQL.getLocationBlockPosed());
+        }, Executors.newCachedThreadPool()).thenRun(() -> {
         });
     }
 
-    public void put(Chunk chunk, Location location) {
-        todo.add(() -> {
+    public void put(Location location) {
+        /*todo.add(() -> {
             // TODO : INJECT
-            RequestsSQL.setLocationBlockPosed(chunk, location);
+            RequestsSQL.setLocationBlockPosed(location);
+        });*/
+        CompletableFuture.runAsync(() -> {
+            // TODO: Requête asynchrone
+            // Effectuez ici votre requête SQL ou toute autre opération longue de manière asynchrone
+            RequestsSQL.setLocationBlockPosed(location);
+        }, Executors.newCachedThreadPool()).thenRun(() -> {
         });
     }
 
-    public void remove(Chunk chunk, Location location) {
-        todo.add(() -> {
+    public void remove(Location location) {
+        /*todo.add(() -> {
            // TODO : REMOVE
-           RequestsSQL.removeLocationBlockPosed(chunk, location);
+           RequestsSQL.removeLocationBlockPosed(location);
+        });*/
+        CompletableFuture.runAsync(() -> {
+            // TODO: Requête asynchrone
+            // Effectuez ici votre requête SQL ou toute autre opération longue de manière asynchrone
+            RequestsSQL.removeLocationBlockPosed(location);
+        }, Executors.newCachedThreadPool()).thenRun(() -> {
         });
     }
 }
